73f9aef9885a9c3381a992f178e69601194757d5
Greg Judd <gregjudd1@hotmail.co.uk>
2017-11-30 16:21:29 +0000

    Add complete countries list

---
+++ vat_countries/countries.py
@@ -0,0 +1,10 @@
+
+import eu_countries as eu
+import usa_countries as usa
+
+COUNTRIES_VAT_MAP = {
+    'germany': eu.GermanyVAT,
+    'uk': eu.UKVAT,
+    'texas': usa.TexasVAT,
+    'colorado': usa.ColoradoVAT
+}
